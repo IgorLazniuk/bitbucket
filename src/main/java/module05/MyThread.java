ba14e06623b7e5366ecf8ce9a6d34ba59cff60ad
igorlazniuk <igorlazniuk@gmail.com>
2017-08-08 16:56:37 +0300

    HomeWork 051

--- src/main/java/module05/MyThread.java
+++ src/main/java/module05/MyThread.java
@@ -3,7 +3,7 @@ package module05;
 import java.util.Arrays;
 
 /**
- * Created by IGOR.LAZNIUK on 08.08.2017. 
+ * Created by IGOR.LAZNIUK on 08.08.2017.
  */
 public class MyThread extends Thread {
     int i;
@@ -11,7 +11,7 @@ public class MyThread extends Thread {
 
     public MyThread(int i) {
         this.i = i;
-    }
+    }  
 
     public void run() {
         synchronized (myArray) {
